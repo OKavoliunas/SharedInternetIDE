ed33e8cdb59d242abee3033486531051ebf97b4c
Oskaras <oskaraskavoliunas@gmail.com>
2024-11-21 22:59:48 +0200

    Adjusted the name of the database from a template "YourDatabaseName" to "OnlineCompilerDB"

---
+++ BlazorServerApp/Migrations/20241118001704_ChangeDBName.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorServerApp.Migrations
+{
+    public partial class ChangeDBName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
