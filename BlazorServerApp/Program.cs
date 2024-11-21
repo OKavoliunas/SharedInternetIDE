ec06933c46a0d497dee63a88093827f43f1c30e4
Oskaras <oskaraskavoliunas@gmail.com>
2024-11-21 23:00:22 +0200

    Set up detailed errorrs

--- BlazorServerApp/Program.cs
+++ BlazorServerApp/Program.cs
@@ -14,6 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+builder.Services.AddServerSideBlazor()
+    .AddCircuitOptions(options => { options.DetailedErrors = true; });
 builder.Services.AddScoped<ProjectManagementService>();
 builder.Services.AddScoped<ProjectDbService>();
 builder.Services.AddSingleton<UserFileService>();
