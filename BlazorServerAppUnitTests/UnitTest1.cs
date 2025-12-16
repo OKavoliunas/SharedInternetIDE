c3b719ac4fbc44669df98f3bb2f6d123604ee116
Oskaras <oskaraskavoliunas@gmail.com>
2025-12-16 15:13:46 +0200

    Initial ND2 changes

---
+++ BlazorServerAppUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace BlazorServerAppUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
